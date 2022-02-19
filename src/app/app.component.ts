5b4ccb57c5c63e982ad9a92283e2fecb0b716062
Fernando Santillan <ferdiego88@gmail.com>
2022-02-19 13:25:07 -0500

    prime NG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre = 'fErnAnDo sAntIlLan';
+  valor = 1000;
+  obj = {
+    nombre: 'Rosa Varas'
+  }
+  mostrarNombre(){
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+    
+    
+  }
 }
