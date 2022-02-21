2cca8abfb7ae78094a41dc9cb62cb4a0a19655d5
Fernando Santillan <ferdiego88@gmail.com>
2022-02-21 06:53:11 -0500

    Decimal Pipes

--- src/app/ventas/pages/numeros/numeros.component.ts
+++ src/app/ventas/pages/numeros/numeros.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-numeros',
   templateUrl: './numeros.component.html',
   styleUrls: ['./numeros.component.css']
 })
-export class NumerosComponent implements OnInit {
+export class NumerosComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ ventasNetas = 2567789.5567;
+ porcentaje = 0.4856;
 
 }
