ab88710b44893c509646043022e003449d8efa7d
Fernando Santillan <ferdiego88@gmail.com>
2022-02-21 11:56:28 -0500

    Async Pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -50,5 +51,14 @@ export class NoComunesComponent {
     this.clientesArray.pop();
     this.clientes.pop();
   }
+
+  //Async Pipe
+  miObservable = interval(1000); // 0,1,2,3 ...
+  valorPromesa = new Promise((resolve, reject)=> {
+
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  })
 
 }
