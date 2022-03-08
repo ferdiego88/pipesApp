172e67779be5b9dab69dcfe1dba6fc0c2c51ab7d
Fernando Santillan <ferdiego88@gmail.com>
2022-03-07 20:12:47 -0500

    vuelaPipe y ordenarArray

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,48 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
   templateUrl: './ordenar.component.html',
   styleUrls: ['./ordenar.component.css']
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent  {
 
+  heroes: Heroe[] = [
+    { 
+      nombre: 'Superman',
+      vuela: true,
+      color: Color.azul
+
+    },
+    { 
+      nombre: 'Batman',
+      vuela: true,
+      color: Color.negro
+
+    }
+    ,
+    { 
+      nombre: 'Robin',
+      vuela: false,
+      color: Color.verde
+
+    },
+    { 
+      nombre: 'DareDevil',
+      vuela: false,
+      color: Color.rojo
+
+    },
+    { 
+      nombre: 'Linterna Verde',
+      vuela: true,
+      color: Color.verde
+
+    }
+  ];
   enMayusculas = false;
   constructor() { }
 
-  ngOnInit(): void {
-  }
 
 }
